c6543d15c9e3fdac4ffaa62fb9dc793a5bf45c1b
Sergey Fesenko <sfesenko@live.ru>
2015-09-23 17:59:46 +0800

    added playing video to video-slide

--- src/slides/video-slide.directive.js
+++ src/slides/video-slide.directive.js
@@ -12,14 +12,35 @@ module.exports = function($timeout) {
         },
         templateUrl: '/slides/video-slide.html',
         link: function(scope, element, attrs, carouselCtrl) {
-            var $elem = angular.element(element);
             var activeClassName = 'active';
+            var $elem = angular.element(element);
+            var $video = $elem.find('video');
+            var video = $video[0];
+            var canPlay = false;
+
+            $video.on('canplaythrough', function() {
+                canPlay = true;
+                video.pause();
+            });
+
+            $video.on('ended', function() {
+                video.currentTime = 0;
+                $timeout(function() {
+                    carouselCtrl.goNext();
+                }, 0);
+            });
+
+            scope.$watch(function() {
+                return canPlay && scope.active;
+            }, function(value) {
+                if (value) {
+                    video.play();
+                }
+            });
 
             scope.$watch('active', function(active) {
                 if (active) {
                     $elem.addClass(activeClassName);
-
-                    $timeout(carouselCtrl.goNext, 3000);
                 } else {
                     $elem.removeClass(activeClassName);
                 }
